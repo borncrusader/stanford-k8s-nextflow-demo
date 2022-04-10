64e187d8606bb818c43e3cefcd41a448245929cf
Srinath Krishna Ananthakrishnan <srinath.krishna@gmail.com>
2022-04-10 15:21:35 -0700

    Adding push directive

--- scripts/01_build_images.sh
+++ scripts/01_build_images.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+if [ "$1" = "--push" ]; then
+    push=1
+fi
+
 # base image
 docker build -f base/Dockerfile \
     -t ghcr.io/borncrusader/stanford-k8s-nextflow-demo-base:latest .
 
-docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-base:latest
+if [ -n "$push" ]; then
+    docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-base:latest
+fi
 
 # preprocessing scripts
 docker build -f preprocessing/00_Dockerfile \
@@ -18,12 +24,16 @@ docker build -f preprocessing/01_Dockerfile \
 docker build -f preprocessing/02_Dockerfile \
     -t ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-02:latest .
 
-docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-00:latest
-docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-01:latest
-docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-02:latest
+if [ -n "$push" ]; then
+    docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-00:latest
+    docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-01:latest
+    docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-pp-02:latest
+fi
 
 # model prediction
 docker build -f models/Dockerfile \
     -t ghcr.io/borncrusader/stanford-k8s-nextflow-demo-models:latest .
 
-docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-models:latest
+if [ -n "$push" ]; then
+    docker push ghcr.io/borncrusader/stanford-k8s-nextflow-demo-models:latest
+fi
